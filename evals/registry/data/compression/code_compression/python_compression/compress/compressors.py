c2cc30eece89fc1b8d6350465d5423312a3ab357
Douglas Monsky <douglas.monsky@gmail.com>
2023-07-13 21:43:17 -0400

    feat(compressors.py): refactor PythonCompressor class #compression-evals
    Refactor the PythonCompressor class in compressors.py. Remove the abstract
    Compressor class and make PythonCompressor a standalone class. Add an
    initializer to PythonCompressor that takes an object and a list of transformers.
    Replace the compress method's input_string parameter with a method that
    iterates over the transformers and applies them to the code. Replace the
    retrieve_code method with a method that returns the raw code. Add a main
    function to test the PythonCompressor class.

--- evals/registry/data/compression/code_compression/python_compression/compress/compressors.py
+++ evals/registry/data/compression/code_compression/python_compression/compress/compressors.py
@@ -1,22 +1,25 @@
-from abc import ABC, abstractmethod
+import ast
+from typing import Any, List, Union
+from transformers import StringTransformer
+from python_code import PythonCode
 
 
-class Compressor(ABC):
+class PythonCompressor:
 
-    @abstractmethod
-    def compress(self, input_string: str) -> str:
-        """Compresses the input string using the compression algorithm."""
-        pass
+    def __init__(self, object_: Any, transformers: List[Union[ast.NodeTransformer, StringTransformer]]) -> None:
+        self.code = PythonCode(object_)
+        self.transformers = transformers
 
-
-class PythonCompressor(Compressor):
-
-    def compress(self, input_string: str) -> str:
-        """Compresses the input string using the compression algorithm."""
-        # Add compression logic here
-        raise NotImplementedError
+    def compress(self) -> str:
+        for transformer in self.transformers:
+            self.code.transform(transformer)
+        return self.code
 
     def retrieve_code(self) -> str:
-        """Retrieve the raw code for this compressor."""
-        # Add code retrieval logic here
-        raise NotImplementedError
+        return self.code.raw_code
+
+
+if __name__ == '__main__':
+    code = PythonCompressor(PythonCode, [])
+    print(code.compress())
+    print(code.retrieve_code())
