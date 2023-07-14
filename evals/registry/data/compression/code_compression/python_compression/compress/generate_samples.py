d520e513e0137ce6aebe63be002e35a5d5eac181
Douglas Monsky <douglas.monsky@gmail.com>
2023-07-14 01:13:46 -0400

    feat(compression-evals): update generate_samples.py for enhanced compression stats #compression-evals
    The generate_samples.py script in the python_compression module has been
    significantly updated. The main function has been renamed to
    generate_simple_compression_sample and now accepts an object as an argument.
    
    Several new transformers have been added to the PythonCompressor object, including
    RemoveEmptyLinesTransformer. The IdentifierRenamingTransformer has been removed.
    
    The function now generates more detailed compression statistics, including the raw
    token count, compressed token count, difference in token count, and difference as a
    percentage. It also retains the original docstrings and the compressed and raw code.
    
    The script now runs the generate_simple_compression_sample function on the
    IndentationStringTransformer object when executed.

--- evals/registry/data/compression/code_compression/python_compression/compress/generate_samples.py
+++ evals/registry/data/compression/code_compression/python_compression/compress/generate_samples.py
@@ -1,21 +1,50 @@
+import tiktoken
+from typing import Any, Dict
 from compressors import PythonCompressor
-from transformers import IdentifierRenamingTransformer,\
-    IndentationStringTransformer, RemoveDocstringsTransformer, TypeHintRemovalTransformer
+from transformers import IndentationStringTransformer, RemoveDocstringsTransformer,\
+    TypeHintRemovalTransformer, RemoveEmptyLinesTransformer
 
 
-def main() -> None:
+def generate_simple_compression_sample(obj_: Any) -> Dict[str, Any]:
     remove_docstrings = RemoveDocstringsTransformer()
-    rename_identifiers = IdentifierRenamingTransformer()
     remove_type_hints = TypeHintRemovalTransformer()
     transform_indentation = IndentationStringTransformer(1)
-    code = PythonCompressor(RemoveDocstringsTransformer,
+    remove_empty_lines = RemoveEmptyLinesTransformer()
+
+    code = PythonCompressor(obj_,
                             [remove_docstrings,
-                             rename_identifiers,
                              remove_type_hints,
-                             transform_indentation
+                             transform_indentation,
+                             remove_empty_lines
                              ])
-    print(code.compress())
+    raw_code = code.code.raw_code
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokenized_raw_code = tokenizer.encode(raw_code)
+    raw_token_count = len(tokenized_raw_code)
+    compressed_code = code.compress().raw_code
+    tokenize_compress_code = tokenizer.encode(compressed_code)
+    compressed_token_count = len(tokenize_compress_code)
+    dif = compressed_token_count - raw_token_count
+    dif_percent = dif / raw_token_count
+
+    original_docs = remove_docstrings.docstrings
+    compression_data = {
+        "compression_stats": {
+                "raw_token_count": raw_token_count,
+                "compressed_token_count": compressed_token_count,
+                "difference": dif,
+                "difference_percent": dif_percent
+                 },
+        "object": obj_,
+        "original_docs": original_docs,
+        "compressed_code": compressed_code,
+        "raw_code": raw_code,
+    }
+    print(f"{obj_.__name__} Compressed.\nRaw token count: {raw_token_count}, "
+          f"Compressed token count: {compressed_token_count}\n"
+          f"Difference: {dif} Tokens ({round(dif_percent, 2) * 100}%)")
+    return compression_data
 
 
 if __name__ == '__main__':
-    main()
+    compression_data = generate_simple_compression_sample(IndentationStringTransformer)
